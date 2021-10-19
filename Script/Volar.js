5fc3936d3d61c5c079e6f607472b1a5a0c10ba4a
Jhojans22 <jhojansvelasquez22@gmail.com>
2021-10-19 17:19:30 -0500

    Se pone paguina de compra y se crea js

---
+++ Script/Volar.js
@@ -0,0 +1,24 @@
+
+var Nombre_Pasajero = document.getElementById('/compra.html/nombre');
+var Telefon = document.getElementById('/compra.html/numero');
+var Comprar = document.getElementById('/compra.html/Enviar');
+var Origen = document.getElementById('/compra.html/salida');
+var Destino = document.getElementById('/compra.html/llegada');
+var Fecha = document.getElementById('/compra.html/Dia-salida');
+
+var Puestos_avion = 3;
+var Puesto_restante = 0;
+var tiket = 0;
+var silla = 0;
+var compra = new Boolean(false);
+var verdad = new Boolean(true);
+
+function myFunction(){
+   location.href="/tiket.html"
+}
+
+
+
+
+
+    
